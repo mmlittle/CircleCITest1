83f5f5824b80f5557c30bdc5ea7d74a85df67568
mike little <mmlittle@yahoo.com>
2018-12-03 16:02:51 -0600

    Initial Commit

---
+++ CircleCITest1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CircleCITest1
+//
+//  Created by MLMillimot on 12/3/18.
+//  Copyright © 2018 Deseo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
